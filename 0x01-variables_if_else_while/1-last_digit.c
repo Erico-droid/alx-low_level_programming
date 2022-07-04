70f722bab92259a5cf1acd499a8ab6050184fcf7
Eric <erickabira@gmail.com>
2022-07-04 04:14:13 -0700

    uppercase to lowercase

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main-Prints letters to lowercase
+ *
+ *Return:Always 0 (success)
+ *
+ */
+int main(void)
+{
+  char c;
+  for(c='a';c<='z';c++)
+    putchar(c);
+  puthchar('\n');
+  return (0)
+    }
